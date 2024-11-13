608b739f9bc7f65f85cf42707bbf31fcb61b2484
euneick <study.euneick@gmail.com>
2024-11-13 11:04:14 +0900

    ModelAndView 객체 생성하여 DispatcherServlet에 전달

--- MVC1stEx/src/main/java/com/spring/ex01/SimpleUrlController.java
+++ MVC1stEx/src/main/java/com/spring/ex01/SimpleUrlController.java
@@ -1,5 +1,16 @@
 package com.spring.ex01;
 
-public class SimpleUrlController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class SimpleUrlController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		return new ModelAndView("index");
+	}
 }
