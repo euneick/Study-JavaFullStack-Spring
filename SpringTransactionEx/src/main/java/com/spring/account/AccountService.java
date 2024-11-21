6e4c71ea77c20ebea47e75d2a6810a02597072a8
euneick <study.euneick@gmail.com>
2024-11-21 10:46:43 +0900

    서비스 클래스 작성

--- SpringTransactionEx/src/main/java/com/spring/account/AccountService.java
+++ SpringTransactionEx/src/main/java/com/spring/account/AccountService.java
@@ -2,4 +2,11 @@ package com.spring.account;
 
 public class AccountService {
 
+	private AccountDAO accountDAO;
+
+	public void setAccountDAO(AccountDAO accountDAO) {
+		this.accountDAO = accountDAO;
+	}
+	
+	
 }
